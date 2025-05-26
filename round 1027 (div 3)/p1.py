5faf39a0b9a84cbdc779fb1248cb5f88b87f5718
TimeB1729 <87745501+TimeB1729@users.noreply.github.com>
2025-05-26 16:03:52 +0530

    fourth commit

---
+++ round 1027 (div 3)/p1.py
@@ -0,0 +1,14 @@
+def ourfunc(t, test_cases):
+    results=[]
+
+    return results
+
+t=int(input())
+test_cases=[]
+for _ in range(t):
+
+    test_cases.append()
+
+results = ourfunc(t,test_cases)
+for res in results:
+    print(res)
